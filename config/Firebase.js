96435c0bdbe85eeb10d0d96f91c3ef84a776c364
maitokuwahara <maito.kuwahara@thebanana.co.jp>
2019-11-14 00:31:10 +0900

    Add gitignore and remove react-native-dotenv

--- config/Firebase.js
+++ config/Firebase.js
@@ -1,13 +1,4 @@
 import firebase from 'firebase'
-import {
-        API_KEY,
-        AUTH_DOMAIN,
-        DATABASE_URL,
-        PROJECT_ID,
-        MESSAGE_SENDER_ID,
-        APP_ID
-    } 
-from 'react-native-dotenv'
 import 'firebase/firestore'
 
 const firebaseConfig = {
